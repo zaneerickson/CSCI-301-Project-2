ffa203dab9f5397daebced47533ec59da83908be
zaneerickson <164396286+zaneerickson@users.noreply.github.com>
2025-08-11 10:30:56 +0000

    6

--- p2t2.cpp
+++ p2t2.cpp
@@ -5,39 +5,22 @@ using namespace std;
 //input/output overloads:
 
 std::istream& operator>>(std::istream& input, item& iobj1) { //overloaded >> operator
-    input >> iobj1.name >> iobj1.quantity >> iobj1.price;
+    input >> iobj1.name >> iobj1.price >> iobj1.quantity;
     return input; //inputs object
 }
 
 std::ostream& operator<<(std::ostream& output, const item& iobj1) { //overloaded << operator
-    output << iobj1.getstring() << " " << iobj1.getint() << " " << iobj1.getfloat();
+    output << iobj1.getstring() << " " << iobj1.getfloat() << " " << iobj1.getint();
     return output; //outputs object
 }
+
 
 //test run
 
 int main() {
     shoppingcart cart;
-
-    // Create some items
-    item apple("Apple", 3, 0.99);
-    item banana("Banana", 2, 0.59);
-    item orange("Orange", 5, 0.79);
-
-    // Add items to cart
-    cart.add(apple);
-    cart.add(banana);
-    cart.add(orange);
-
-    cout << "Cart total after adding items: $" << cart.getTotalPrice() << endl;
-
-    // Remove one item
-    cart.remove(banana);
-
-    cout << "Cart total after removing banana: $" << cart.getTotalPrice() << endl;
-
-    // Clear cart
-    cart.clear();
-
-    cout << "Cart total after clearing: $" << cart.getTotalPrice() << endl;
+    item i1;
+    cout << "Welcome to XXX SHOPPING CENTER." << endl << " Enter the item you selected as the following order: " << endl << " name unitPrice quantity" << endl << "(Name can not contain any space. Otherwise errors happen!)" << endl;
+    cin >> i1;
+    cout << i1;
 }
